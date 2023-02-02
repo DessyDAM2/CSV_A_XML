5d125fd509dc9897feb9dc62972ac6e61431d3f3
Alumno_FP <200076@iessanalberto.com>
2023-02-02 08:51:00 +0100

    Toma todo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import com.aspose.cells.Workbook;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Workbook workbook = new Workbook("src/main/resources/centrosFP2.csv");
+        workbook.save("target/centrosFP2.xml");
+        Workbook workbook2 = new Workbook("src/main/resources/proyectos.csv");
+        workbook2.save("target/proyectos.xml");
     }
 }
